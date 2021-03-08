9dfae5c6e9d484b4b4b59cb42cbb0c4323c1f5d9
Zach von Grote <zvongrote@gmail.com>
2021-03-07 19:46:13 -0800

    Set the app to be fullscreen
    See the comment in the main activity for a better way to do this once androidx.core version 1.5.0 is stable

--- app/src/main/java/com/zachvg/chessclock/MainActivity.kt
+++ app/src/main/java/com/zachvg/chessclock/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.zachvg.chessclock
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        setFullscreen()
+        supportActionBar?.hide()
+    }
+
+    // The systemUiVisibility way of enabling fullscreen is deprecated; the new way is
+    // to use the window insets controller. However, the compatibility classes are in
+    // beta so I don't want to use them yet. See this post for an example of how they
+    // should work once they're stable:
+    // https://stackoverflow.com/a/64828028/9633447
+    @Suppress("DEPRECATION")
+    private fun setFullscreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.run {
+                hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                    // Set the content to appear under the system bars so that the
+                    // content doesn't resize when the system bars hide and show.
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    // Hide the nav bar and status bar
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        }
     }
 }
