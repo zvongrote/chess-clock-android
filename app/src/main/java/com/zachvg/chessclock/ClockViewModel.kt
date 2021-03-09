27f4ab0f9e24d4b130c65a2ec45249479bba3ee5
Zach von Grote <zvongrote@gmail.com>
2021-03-09 00:22:10 -0800

    Adjusted the clock UI.
    Added labels identifying which player is on which side of the screen. This will
    correspond to the player options in the game settings page.

--- app/src/main/java/com/zachvg/chessclock/ClockViewModel.kt
+++ app/src/main/java/com/zachvg/chessclock/ClockViewModel.kt
@@ -2,5 +2,27 @@ package com.zachvg.chessclock
 
 import androidx.lifecycle.ViewModel
 
+private enum class GameState {
+    NOT_STARTED,
+    IN_PROGRESS,
+    PAUSED,
+    FINISHED
+}
+
+private enum class Player {
+    PLAYER_1,
+    PLAYER_2
+}
+
 class ClockViewModel : ViewModel() {
+
+    private var currentPlayer = Player.PLAYER_1
+
+    fun onPlayer1ButtonClick() {
+
+    }
+
+    fun onPlayer2ButtonClick() {
+
+    }
 }
