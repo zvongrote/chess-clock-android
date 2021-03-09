0efb8457fd764de9a4919c2d9f3bebd27d5950f9
Zach von Grote <zvongrote@gmail.com>
2021-03-09 03:03:02 -0800

    Added a count down timer class
    This class uses the built-in count down timer. It's only as accurate as
    the countdown interval, which could result in one player getting more
    time than the other. This needs to be fixed in the future, but it will
    work for now. See the comments in the class for more explanation.
    
    blah

---
+++ app/src/main/java/com/zachvg/chessclock/PlayerCountdownTimer.kt
@@ -0,0 +1,73 @@
+package com.zachvg.chessclock
+
+import android.os.CountDownTimer
+
+// Adds a rudimentary pause feature to the built in Android CountDownTimer
+
+// This implementation isn't very accurate since the timeLeftMillis variable
+// is only updated during the onTick method, which depends on the countdown
+// interval. The problem occurs when the timer is paused in between calls to
+// onTick; the time elapsed after a call to onTick and the pause is lost,
+// which results in slight increases the the actual time.
+//
+// For example: if the time starts at 10 seconds and the countdown interval
+// is 1 second, and it's paused at 9.1 seconds, then the onTick event hasn't
+// fired and the timeLeftMillis will still be set to 10 seconds. So, when
+// it's unpaused the timer will start at 10 seconds again, causing a total
+// increase of 0.9 seconds in the total time.
+//
+// Using a shorter countdown interval helps the reduce the problem, but it's
+// possible that slower hardware may not be able to handle very short interval.
+// The default is set to 100ms right now.
+//
+// This could be an especially bad problem for the a chess match that has a
+// an increment since a faster player could end up with much more time than
+// a slower one. This needs to be fixed in the future, but it will work for now.
+// One possible solution:
+// https://stackoverflow.com/a/9802426/9633447
+abstract class PlayerCountDownTimer(millisInFuture: Long, private var countDownInterval: Long = 100L) {
+    private var totalTimeMillis = millisInFuture
+    private var timeLeftMillis = millisInFuture
+
+    private var timer = newCountDownTimer(millisInFuture, countDownInterval)
+
+    fun start() {
+        timer.start()
+    }
+
+    fun restart() {
+        timer = newCountDownTimer(totalTimeMillis, countDownInterval)
+    }
+
+    fun pause() {
+        timer.cancel()
+        timer = newCountDownTimer(timeLeftMillis, countDownInterval)
+    }
+
+    fun cancel() {
+        timer.cancel()
+    }
+
+    fun setTime(newMillisInFuture: Long) {
+        timer.cancel()
+        timer = newCountDownTimer(newMillisInFuture, countDownInterval)
+    }
+
+    private fun newCountDownTimer(millisInFuture: Long, countDownInterval: Long): CountDownTimer {
+        return object : CountDownTimer(millisInFuture, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeftMillis = millisInFuture
+                this@PlayerCountDownTimer.onTick()
+            }
+
+            override fun onFinish() {
+                this@PlayerCountDownTimer.onFinish()
+            }
+
+        }
+    }
+
+    abstract fun onFinish()
+
+    abstract fun onTick()
+}
