f682fd61b0708f3e813a30cc8e96cb34c4161cb4
Zach von Grote <zvongrote@gmail.com>
2021-03-15 02:59:16 -0700

    Added a dialog class for confirming clock reset

---
+++ app/src/main/java/com/zachvg/chessclock/clock/ResetDialog.kt
@@ -0,0 +1,24 @@
+package com.zachvg.chessclock.clock
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import com.zachvg.chessclock.R
+
+/**
+ * Dialog that confirms if the user wants to reset the clock
+ */
+class ResetDialog(private val onPositiveClickListener: DialogInterface.OnClickListener) : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return requireActivity().let {
+            val builder = AlertDialog.Builder(it).apply {
+                setMessage(R.string.reset_clock)
+                setPositiveButton(R.string.yes, onPositiveClickListener)
+            }
+
+            builder.create()
+        }
+    }
+}
